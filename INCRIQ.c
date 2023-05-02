7ad0e27b401b2df936463ae26c3acfb7f37692c2
piyushkhurana2019 <96003210+piyushkhurana2019@users.noreply.github.com>
2023-05-02 23:57:30 +0530

    Create INCRIQ.c

---
+++ INCRIQ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int X;
+	scanf("%d",&X);
+	
+	if(X+7>170)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	    printf("no");
+	}
+	return 0;
+}
